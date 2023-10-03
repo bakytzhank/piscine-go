f47afbe38b02090e4e88cab2018fea105ceaf02f
bakytzhank <68074353+bakytzhank@users.noreply.github.com>
2023-10-03 19:48:31 +0600

    Add files via upload

--- piscine-go/Quest05/printnbrbase.go
+++ piscine-go/Quest05/printnbrbase.go
@@ -4,6 +4,13 @@ import "github.com/01-edu/z01"
 
 func PrintNbrBase(nbr int, base string) {
 	n := len(base)
+	for _, l := range base {
+		if l == '-' || l == '+' {
+			z01.PrintRune('N')
+			z01.PrintRune('V')
+			return
+		}
+	}
 	for i := 0; i < len(base); i++ {
 		for j := i + 1; j < len(base); j++ {
 			if base[i] == base[j] {
