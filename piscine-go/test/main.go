af866564b48b9ef5e8ce51b820c119728090ef72
Bakytzhan Kaziyev <baky@Bakytzhans-MacBook-Pro.local>
2023-10-03 23:31:32 +0600

    update

--- piscine-go/test/main.go
+++ piscine-go/test/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"github.com/01-edu/z01"
 	"piscine"
 )
 
 func main() {
-	s := []int{5,1,9,3,7,0}
-	piscine.SortIntegerTable(s)
-	fmt.Println(s)
+	piscine.PrintNbrBase(-9223372036854775808, "0123456789")
+	z01.PrintRune('\n')
+	piscine.PrintNbrBase(-125, "01")
+	z01.PrintRune('\n')
+	piscine.PrintNbrBase(125, "0123456789ABCDEF")
+	z01.PrintRune('\n')
+	piscine.PrintNbrBase(-125, "choumi")
+	z01.PrintRune('\n')
+	piscine.PrintNbrBase(125, "aa")
+	z01.PrintRune('\n')
 }
