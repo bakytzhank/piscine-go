626df6f43b74a7c404b2d7678b6eb53d7d14c183
bakytzhank <68074353+bakytzhank@users.noreply.github.com>
2023-10-10 19:09:21 +0600

    Update main.go

--- piscine-go/Quest08/cat/main.go
+++ piscine-go/Quest08/cat/main.go
@@ -1,35 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
+	executablePath := os.Args[0]
+	executableDir := strings.TrimSuffix(executablePath, "/cat/main")
+
 	if len(os.Args) == 1 {
-		// If no arguments are provided, read from stdin and write to stdout
+		// If no arguments are provided, read from stdin and print to stdout.
 		_, err := io.Copy(os.Stdout, os.Stdin)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+			os.Stderr.WriteString("ERROR: " + err.Error() + "\n")
 			os.Exit(1)
 		}
 	} else {
-		// If arguments are provided, treat them as file paths
-		for _, arg := range os.Args[1:] {
-			filePath := ".." + "/" + arg
+		// If arguments are provided, open and read each file, then print its contents.
+		for _, filename := range os.Args[1:] {
+			filePath := executableDir + "/" + filename
 			file, err := os.Open(filePath)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+				os.Stderr.WriteString("ERROR: " + err.Error() + "\n")
 				os.Exit(1)
 			}
-			defer file.Close()
-
 			_, err = io.Copy(os.Stdout, file)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+				os.Stderr.WriteString("ERROR: " + err.Error() + "\n")
 				os.Exit(1)
 			}
+			file.Close()
 		}
 	}
 }
